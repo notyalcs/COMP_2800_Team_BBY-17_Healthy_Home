87f71744820ba1d0ec0921ec1545645b552241ad
daniilmol <daniilmik@gmail.com>
2020-05-04 13:13:54 -0700

    Renamed and fixed some bugs

---
+++ scripts/showRoutine.js
@@ -0,0 +1,16 @@
+let exercises = ["Crunches", "Pushups", "Squats"];
+let stretches = ["Stretch 1", "Stretch 2","Stretch 3"];
+
+function display(id){
+    if(window.location.href.indexOf("exercise") > -1){
+        $("#displayRoutines").text(exercises[id]);
+    }else if(window.location.href.indexOf("stretch") > -1){
+        $("#displayRoutines").text(stretches[id]);
+    }
+}
+
+$(".arrow").on("click", function(){ 
+    $(".arrow").removeClass("down right").addClass("right");
+    $(this).removeClass("right").addClass("down");
+    display($(this).attr('id'));                 
+});
